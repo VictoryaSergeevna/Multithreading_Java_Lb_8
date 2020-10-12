1d27bc91b00417c194a208fa449d9d442833773d
VictoryaSergeevna <vikusyachk@gmail.com>
2020-10-12 15:26:59 +0300

    This last commit for me

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+        new MyThread(obj).start();
+        new MyThread(obj).start();
+    }
+}
